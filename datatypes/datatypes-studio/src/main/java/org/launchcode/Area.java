d8f1d20d553adcb0f34fa9533d1d54c786741ee0
anamindful <anamindful@gmail.com>
2023-08-21 19:53:36 -0500

    StudioClass1Java

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,21 @@
+package org.launchcode;
+
+import  java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+
+        Scanner input;
+
+        input = new Scanner(System.in);
+
+        System.out.println("Enter a radius:");
+
+        double radius = input.nextDouble();
+
+        input.close();
+
+        double area = 3.14 * radius * radius;
+
+        System.out.println("The area of a circle of a radius " + radius + " is: " + area);
+    }
+}
